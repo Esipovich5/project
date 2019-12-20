122559e43cac8dc93e7e73ea269ceb4fd981513b
Esipovich5 <www.Esipovich@gmail.com>
2019-12-20 22:16:00 +0200

    commit dz

--- DZ6.js
+++ DZ6.js
@@ -1,3 +1,4 @@
+/*
 let warn = {
     danger: function(){
         alert('Danger!')
@@ -9,5 +10,36 @@ let warn = {
 
 warn.danger();
 warn.fire();
+*/
 
 
+let Car = {
+    make:'USA',
+    displayMake:function(){
+        console.log(this.make);
+    },
+    model:'BMW',
+    displayModel:function(){
+        console.log(this.model);
+    },
+    
+    year: 2003,
+    displayYear:function(){
+        console.log(this.year);
+    },
+    color: 'black',
+    displayColor:function(){
+        console.log(this.color);
+    },
+    weight: 3000,
+    displayWeight:function(){
+        console.log(this.weight);
+    },
+};
+
+
+
+let Ford = Object.create(Car);
+Ford.year = 2015;
+Ford.displayYear();
+Ford.displayColor();
